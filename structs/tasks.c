d9de49c338e1ac6abebb64e9481e5f0df1052f21
ousmane <sangaryousmane@gmail.com>
2023-04-27 03:21:07 +0800

    Last function done, going to bed now, I will continue tomorrow!

--- structs/tasks.c
+++ structs/tasks.c
@@ -48,10 +48,20 @@ dog_t *new_dog(char *name, float age, char *owner){
     dog->age=age;
     return dog;
 }
+
+void free_dog(dog_t *d){
+    if (d==NULL)
+        return;
+
+    free(d->name);
+    free(d->owner);
+    free(d);
+}
 int test() {
     dog_t *my_dog;
 
     my_dog = new_dog("Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    free_dog(my_dog);
     return 0;
 }
