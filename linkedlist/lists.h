83f5073b8c879df5531ce5834d194a1aca22b1fc
ousmane <sangaryousmane@gmail.com>
2023-04-27 14:37:24 +0800

    Add 2-add_node.c

--- linkedlist/lists.h
+++ linkedlist/lists.h
@@ -13,4 +13,5 @@ typedef struct list_s{
 
 /* Function Prototypes */
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif /* __LISTS__ */
--- linkedlist/tasks.c
+++ linkedlist/tasks.c
@@ -18,31 +18,78 @@ size_t print_list(const list_t *h) {
     return node_count;
 }
 
+
+/* TASK 2 */
+// Write a function that returns the number of elements in a linked list_t list.
+size_t list_len(const list_t *h){
+
+    size_t num_elements = 0;
+
+    while (h != NULL){
+        if (h->str != NULL && (*h).len > 0)
+            num_elements +=1;
+
+        h = h->next;
+    }
+    return num_elements;
+}
+
+// TASK 3 */
+// Write a function that adds a new node at the beginning of a list_t list.
+list_t *add_node(list_t **head, const char *str){
+
+    list_t *new_list;
+    int i;
+    unsigned int length = 0;
+
+    for (i = 0; str[i] !='\0'; i++){
+        length++;
+    }
+
+    new_list = malloc(sizeof(list_t));
+
+    if(new_list){
+        new_list->str= strdup(str);
+        new_list->len=length;
+        new_list->next=(*head);
+        (*head)=new_list;
+        return new_list;
+    } else{
+        return NULL;
+    }
+}
 int test_me() {
     list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL) {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = print_list(head);
-    printf("-> %zu elements\n", n);
-
-    printf("\n");
-    free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %zu elements\n", n);
-
-    free(new);
+    head = NULL;
+    add_node(&head, "Alexandro");
+    add_node(&head, "Asaia");
+    add_node(&head, "Augustin");
+    add_node(&head, "Bennett");
+    add_node(&head, "Bilal");
+    add_node(&head, "Chandler");
+    add_node(&head, "Damian");
+    add_node(&head, "Daniel");
+    add_node(&head, "Dora");
+    add_node(&head, "Electra");
+    add_node(&head, "Gloria");
+    add_node(&head, "Joe");
+    add_node(&head, "John");
+    add_node(&head, "John");
+    add_node(&head, "Josquin");
+    add_node(&head, "Kris");
+    add_node(&head, "Marine");
+    add_node(&head, "Mason");
+    add_node(&head, "Praylin");
+    add_node(&head, "Rick");
+    add_node(&head, "Rick");
+    add_node(&head, "Rona");
+    add_node(&head, "Siphan");
+    add_node(&head, "Sravanthi");
+    add_node(&head, "Steven");
+    add_node(&head, "Tasneem");
+    add_node(&head, "William");
+    add_node(&head, "Zee");
+    print_list(head);
     return 0;
 }
