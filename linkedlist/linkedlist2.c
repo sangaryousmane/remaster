cbb7044251519b301629c35d177bb517216bf466
ousmane <sangaryousmane@gmail.com>
2023-04-30 16:37:46 +0800

    Add a new list to the list

--- linkedlist/linkedlist2.c
+++ linkedlist/linkedlist2.c
@@ -3,11 +3,11 @@
 
 
 // Task 1
-size_t print_listint(const listint_t *h){
+size_t print_listint(const listint_t *h) {
     size_t num_of_nodes = 0;
 
 
-    for (; h !=NULL; num_of_nodes++, h = h->next){
+    for (; h != NULL; num_of_nodes++, h = h->next) {
         printf("%d\n", h->n);
     }
 
@@ -15,41 +15,69 @@ size_t print_listint(const listint_t *h){
 }
 
 // Task 2
-size_t listint_len(const listint_t *h){
+size_t listint_len(const listint_t *h) {
     size_t num_el = 0;
 
-    for (; h !=NULL; num_el +=1){
+    for (; h != NULL; num_el += 1) {
         h = h->next;
     }
     return num_el;
 }
 
 // Task 3
-listint_t *add_nodeint(listint_t **head, const int n){
+listint_t *add_nodeint(listint_t **head, const int n) {
     listint_t *new_list;
 
-    new_list= malloc(sizeof(listint_t));
+    new_list = malloc(sizeof(listint_t));
 
-    if (new_list != NULL){
-        new_list->n=n;
-        new_list->next=(*head);
-        (*head)=new_list;
+    if (new_list != NULL) {
+        new_list->n = n;
+        new_list->next = (*head);
+        (*head) = new_list;
         return new_list;
     }
     return NULL;
 }
-int show_it(){
+
+// Task 4
+listint_t *add_nodeint_end(listint_t **head, const int n) {
+    listint_t *new_list;
+    listint_t *temp_var = (*head);
+
+    new_list = malloc(sizeof(listint_t));
+
+    if (new_list == NULL) {
+        return NULL;
+    }
+    else {
+        new_list->n = n;
+        new_list->next = NULL;
+        if (*head == NULL) {
+            (*head) = new_list;
+            return new_list;
+        }
+
+        while (temp_var->next) {
+            temp_var = temp_var->next;
+        }
+        temp_var->next = new_list;
+        return new_list;
+    }
+
+}
+
+int show_it() {
     listint_t *head;
 
     head = NULL;
-    add_nodeint(&head, 0);
-    add_nodeint(&head, 1);
-    add_nodeint(&head, 2);
-    add_nodeint(&head, 3);
-    add_nodeint(&head, 4);
-    add_nodeint(&head, 98);
-    add_nodeint(&head, 402);
-    add_nodeint(&head, 1024);
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
     print_listint(head);
     return 0;
 }
